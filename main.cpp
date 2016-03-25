fc68794553c3b11b4a9ea59975a38f9e14917581
ecurioso <ernest.curioso@gmail.com>
2016-03-24 21:21:06 -0700

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QDesktopWidget>
-#include <QDebug>
-#include <QtCore>
-#include <QTimer>
-#include "uav.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QDesktopWidget dw;
 
-    int x=dw.width()*0.95;
-    int y=dw.height()*0.8;
-    w.setFixedSize(x,y);
-
-    w.show();
-
-    Uav uav1;
+    w.showMaximized();
 
     return a.exec();
 }
